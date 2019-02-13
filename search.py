695ecd2984f564aeaa52597022e644b78889f181
Giovani Gomes <giovanigomes97@gmail.com>
2019-02-13 20:04:51 -0200

    gitignore adicionado, busca alterada, encoding corrigido

--- search.py
+++ search.py
@@ -2,14 +2,14 @@ from googlesearch import search
 import csv
 
 # Lê o arquivo com o nome das empresas a serem buscadas
-with open('empresas.csv') as f:
+with open('empresas.csv',newline='', encoding='utf-8') as f:
     dados = csv.reader(f)
     empresas = [row[0] for row in dados]
 print(empresas)
 
 for empresa in empresas:
     print('Resultados empresa "'+empresa+'":')
-    for j in search(empresa, tld="co.in", num=10, stop=1, pause=2): 
+    for j in search(empresa, tld="co.in",lang="pt-br",tbs="qdr:m", num=10, stop=1, pause=2): #tbs: limite de tempo
         print(j) 
     
     print('\n\n')
