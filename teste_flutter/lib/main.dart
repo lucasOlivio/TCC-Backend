aa5e9a85af83cf86024419b7d4b7d20d695849c8
Giovani Gomes <giovanigomes97@gmail.com>
2019-03-03 17:51:34 -0300

    Formulário realocado e validação inserida

--- teste_flutter/lib/main.dart
+++ teste_flutter/lib/main.dart
@@ -3,102 +3,106 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Teste',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Teste TCC'),
+      home: HomePage(title: 'Flutter Teste TCC'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false; //Validação automática ativada caso form. incorreto
+  String _email;
+  String _password;
+
+  Widget formLogin(){
+    return new Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'E-mail'
+            ),
+            keyboardType: TextInputType.emailAddress,
+            validator: (String arg) {
+              if((arg.length == 0) || !(arg.contains('@'))) {
+                return 'Digite um e-mail válido';
+              }else {
+                return null;
+              }
+            },
+            onSaved: (String val) {
+              _email = val;
+            },
+          ),
+
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Senha'
+            ),
+            obscureText: true,
+            validator: (String arg) {
+              if(arg.length < 6) {
+                return 'Digite uma senha válida';
+              }else{
+                return null;
+              }
+            },
+            onSaved: (String val) {
+              _password = val;
+            },
+          ),
+
+          RaisedButton(
+            child: const Text('Entrar', semanticsLabel: 'Entrar'),
+            color: Colors.lightGreen,
+            splashColor: Colors.blueGrey,
+            onPressed: _validarLogin,
+          ),
+    ]);
+  }
+
+  void _validarLogin(){
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+    }else{
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+
+    print("Login : $_email e senha: $_password");
+    //Inserir função de login
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'E-mail'
-              ),
-            ),
-            
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Senha'
-              ),
-              obscureText: true,
-            ),
-
-            RaisedButton(
-              child: const Text('Entrar', semanticsLabel: 'Entrar'),
-              color: Colors.lightGreen,
-              splashColor: Colors.blueGrey,
-              onPressed: (){
-                
-              },
+            new Form(
+              key: _formKey,
+              autovalidate: _autoValidate,
+              child: formLogin(),
             ),
           ],
         ),
