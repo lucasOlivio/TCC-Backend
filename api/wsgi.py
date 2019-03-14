f615f885658379061260b9f1d1b47eb28f6462f6
iedashboard <suporte@gmail.com.br>
2019-03-14 18:19:15 -0300

    Correção de configuração parawhitenoise

--- api/wsgi.py
+++ api/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
-from django.core.wsgi import get_wsgi_application
+# import os
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
