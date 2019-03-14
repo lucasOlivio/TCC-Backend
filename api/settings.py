7bc7e6bcffc9ceffaaa83eb6beeb1894a7b8bdde
iedashboard <suporte@gmail.com.br>
2019-03-14 19:28:28 -0300

    Configurações do banco de dados

--- api/settings.py
+++ api/settings.py
@@ -16,7 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+SECRET_KEY = dj_database_url.config('SECRET_KEY')
+DEBUG = dj_database_url.config('DEBUG', default=False, cast=bool)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -71,8 +72,7 @@ TEMPLATES = [
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-SECRET_KEY = dj_database_url.config('SECRET_KEY')
-DEBUG = dj_database_url.config('DEBUG', default=False, cast=bool)
+
 DATABASES = {
     'default': dj_database_url.config(
         default=dj_database_url.config('DATABASE_URL')
