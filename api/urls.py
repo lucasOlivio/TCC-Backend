1c026346a87e6c510de9dc15e828095f01db8aee
iedashboard <suporte@gmail.com.br>
2019-03-14 22:40:16 -0300

    Configurações django heroku

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from invest_back_end import views
+from django.contrib import admin
 
 # URLs redirecting to views functions or apps
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
