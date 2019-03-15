8050e75965f9f431cff299c47526329773d685bf
iedashboard <suporte@gmail.com.br>
2019-03-14 22:54:58 -0300

    Configurações django heroku

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,8 +1,16 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
 import os
-# from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+from django.core.wsgi import get_wsgi_application
 
-# application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+application = get_wsgi_application()
