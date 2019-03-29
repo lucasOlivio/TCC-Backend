cdc3bc9f53b36cfefc384ad9d9fc795070e47b7f
lucasOlivio <lucas27_olivio@hotmail.com>
2019-03-29 19:43:35 -0300

    Adicionados dados para teste com gráfico em tela principal

--- invest_back_end/views.py
+++ invest_back_end/views.py
@@ -9,5 +9,14 @@ class MainGraphView(APIView):
 
     # Receives the request and returns the json with the message
     def get(self, request):
-        content = {'message': 'Hello, World!'}
+        resp = True
+        data = [
+            [1,2,3,4,5,6,7,8,9,10],
+            [2,1,5,6,4,9,7,0,5,3],
+            [4,4,6,8,9,9,2,10,10,10],
+        ]
+        content = {
+            'resp': resp,
+            'data':data
+        }
         return Response(content)
