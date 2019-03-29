cdc1a060f98fb5815443b797d2a339a7e0a4a846
lucasOlivio <lucas27_olivio@hotmail.com>
2019-03-29 19:29:09 -0300

    Criada nova view para retornar o gráfico da tela principal

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls import include, url
 # URLs redirecting to views functions or apps
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('main-graph/', views.MainGraphView.as_view(), name='main_graph'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- invest_back_end/views.py
+++ invest_back_end/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
-class HelloView(APIView):
-    '''Simple class to return a hello message'''
+class MainGraphView(APIView):
+    '''Simple class to return the data of the user'''
     permission_classes = (IsAuthenticated,)
 
     # Receives the request and returns the json with the message
