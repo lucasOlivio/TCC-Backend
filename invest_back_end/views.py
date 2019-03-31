59ddf0c428f210da152372e06b2c3a7e1cbc33b5
lucasOlivio <lucas27_olivio@hotmail.com>
2019-03-31 16:36:18 -0300

    Correção de importação de função

--- invest_back_end/views.py
+++ invest_back_end/views.py
@@ -1,10 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
-# Adding path to the functions folder
-import sys
-sys.path.insert(0, 'functions/')
 from shares import Shares
 
 
