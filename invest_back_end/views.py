4fb93d65ac9fb9893ae3b4cbfe3f0c8c948f3bcb
lucasOlivio <lucas27_olivio@hotmail.com>
2019-03-31 16:39:34 -0300

    Correção de importação de função

--- invest_back_end/views.py
+++ invest_back_end/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from shares import Shares
+from invest_back_end.shares import Shares
 
 
 class MainGraphView(APIView):
