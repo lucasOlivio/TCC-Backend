815b4997a6fdceef47be2effb142db970a74f221
lucasOlivio <lucas27_olivio@hotmail.com>
2019-03-31 16:49:42 -0300

    Adicionado iexfinance aos requirement

--- invest_back_end/views.py
+++ invest_back_end/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from invest_back_end.shares import Shares
+from pandas.tseries.offsets import BDay
+import datetime
 
 
 class MainGraphView(APIView):
