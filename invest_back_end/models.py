50549d0fb28832a468d5afeeaa63f70317987cec
lucasOlivio <lucas27_olivio@hotmail.com>
2019-06-24 19:19:39 -0300

    Adicionado novo model para guardar as informações extras do usuário

--- invest_back_end/models.py
+++ invest_back_end/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# Create new table for the extra info about the user
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.TextField(max_length=15, blank=True)
+    age = models.IntegerField(null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# Update extra info about the user
+def update_profile(request, user_id, gender, age):
+    user = User.objects.get(pk=user_id)
+    user.profile.gender = gender
+    user.profile.age = age
+    user.save()
