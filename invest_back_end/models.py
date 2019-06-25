24ee55e568be0c0821d997275032053864161469
lucasOlivio <lucas27_olivio@hotmail.com>
2019-06-25 20:42:00 -0300

    Adicionado signal post_save

--- invest_back_end/models.py
+++ invest_back_end/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create new table for the extra info about the user
 class Profile(models.Model):
