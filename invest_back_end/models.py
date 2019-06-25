c838098e59a6e7f5e3bd07212fd875177ffa80dd
lucasOlivio <lucas27_olivio@hotmail.com>
2019-06-25 20:47:44 -0300

    Comentarios para debug do codigo

--- invest_back_end/models.py
+++ invest_back_end/models.py
@@ -9,18 +9,18 @@ class Profile(models.Model):
     gender = models.TextField(max_length=15, blank=True)
     age = models.IntegerField(null=True)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
-# Update extra info about the user
-def update_profile(request, user_id, gender, age):
-    user = User.objects.get(pk=user_id)
-    user.profile.gender = gender
-    user.profile.age = age
-    user.save()
+# # Update extra info about the user
+# def update_profile(request, user_id, gender, age):
+#     user = User.objects.get(pk=user_id)
+#     user.profile.gender = gender
+#     user.profile.age = age
+#     user.save()
