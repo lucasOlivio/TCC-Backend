f4128845b75672b1c561c6fa0a7f58bc8dc53189
lucasOlivio <lucas27_olivio@hotmail.com>
2019-06-25 22:57:20 -0300

    Ajuste options

--- invest_back_end/migrations/0001_initial.py
+++ invest_back_end/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
-        options={
+        options = {
             'managed': True,
         },
     ]
