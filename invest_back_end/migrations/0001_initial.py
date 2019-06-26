84d7586a4ec35422305a4bbb3fc1b15ded987356
lucasOlivio <lucas27_olivio@hotmail.com>
2019-06-25 22:59:04 -0300

    Removido options

--- invest_back_end/migrations/0001_initial.py
+++ invest_back_end/migrations/0001_initial.py
@@ -23,7 +23,4 @@ class Migration(migrations.Migration):
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
-        options = {
-            'managed': True,
-        },
     ]
